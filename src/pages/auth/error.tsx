120ac75c21f603b4d7f06cc1f18f8508a9c26a38
RhoumaAbir <160128554+RhoumaAbir@users.noreply.github.com>
2024-10-23 18:40:12 +0100

    Update error.tsx
    modifier pour deploy

--- src/pages/auth/error.tsx
+++ src/pages/auth/error.tsx
@@ -5,7 +5,7 @@ interface AuthErrorProps {
   export default function AuthError({ error }: AuthErrorProps) {
     return (
       <div>
-        <h1>Erreur d'authentification</h1>
+        <h1>Erreur authentification</h1>
         <p>{error || 'Une erreur est survenue lors de authentification.'}</p>
       </div>
     );
